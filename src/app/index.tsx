fc52c3319ea0b72a8222249383b0ccad2ca67cef
Konstantinov Aleksey <KonstantinovAI@enplus.digital>
2024-08-28 15:43:02 +0800

    фикс обработка валидности данных

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -10,6 +10,7 @@ export const App = () => {
 	const [data, setData] = useState<IIngredients[]>([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [hasError, setHasError] = useState(false);
+	const [codeError, setCodeError] = useState('');
 
 	useEffect(()=>{
 		if(!isLoading && data.length === 0){
@@ -20,7 +21,14 @@ export const App = () => {
 	const getIngredients = () => {
 		setIsLoading(true);
 		fetch(url)
-		.then(res => res.json())
+		.then(res => {
+			if (res.ok) {
+				return res.json();
+			}
+			// записываю в переменую, чтоб если делать обработку то от изменения ее
+			return Promise.reject(setCodeError(`${res.status}`));
+
+		})
 		.then(data => setData(data.data))
 		.catch(e => setHasError(true))
 		.finally(() => setIsLoading(false));
