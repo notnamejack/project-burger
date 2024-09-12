dee6c8cb31397346d73bd5846b7b857d368fec2c
Konstantinov Aleksey <KonstantinovAI@enplus.digital>
2024-09-12 16:51:52 +0800

    сделал функцию выбранных

--- src/components/igredients/igredients.tsx
+++ src/components/igredients/igredients.tsx
@@ -1,8 +1,6 @@
 
 import clsx from 'clsx';
 import s from './igredients.module.scss';
-import { useSelector } from "react-redux";
-import { RootState } from "../../services/store";
 import Ingredient from "../ingredient";
 import { useGetIngredientsQuery } from '../../services/ingredients/api';
 
@@ -15,15 +13,12 @@ export function Ingredients ({title, type}: IIngredientItem){
 
 	const {data} = useGetIngredientsQuery();
 
-	const selectIngredients = useSelector((state: RootState) => state.ingredientsSelect.items)
-	const bun = useSelector((state: RootState) => state.ingredientsSelect.bun)
-
 	return (
 		<>
 			<p className="text text_type_main-medium mb-6">{title}</p>
 			<ul className={`${clsx(s.items)} pl-1`}>
 				{data?.data.filter(i => i.type === type).map(item =>
-					<Ingredient key={item._id} ingredient={item} count={item === bun ? 2 : selectIngredients.filter(i => i._id === item._id).length}/>
+					<Ingredient key={item._id} ingredient={item}/>
 				)}
 			</ul>
 		</>
--- src/components/ingredient/ingredient.tsx
+++ src/components/ingredient/ingredient.tsx
@@ -3,17 +3,21 @@ import { Counter, CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-c
 import clsx from 'clsx';
 import s from './igredient.module.scss';
 import { IIngredients } from "../../data/ingredients";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useDrag } from "react-dnd";
 import { openModal } from "../../services/ingredients-details-splice/reducer";
+import { useMemo } from "react";
+import { RootState } from "../../services/store";
 
 interface IIngredient {
-	ingredient: IIngredients,
-	count: number
+	ingredient: IIngredients
 }
 
-export function Ingredient ({ingredient, count}: IIngredient){
+export function Ingredient ({ingredient}: IIngredient){
 	const dispatch = useDispatch()
+
+	const selectIngredients = useSelector((state: RootState) => state.ingredientsSelect.items)
+	const bun = useSelector((state: RootState) => state.ingredientsSelect.bun)
 
 	const [{ opacity }, ref] = useDrag({
 		type: 'ingredient',
@@ -22,6 +26,17 @@ export function Ingredient ({ingredient, count}: IIngredient){
 			opacity: monitor.isDragging() ? 0.5 : 1
 			})
 	  });
+
+	const count = useMemo(() => {
+		var count = 0;
+		if(ingredient.type !== 'bun')
+			count = selectIngredients.filter(i => i._id === ingredient._id).length;
+
+		if(bun?._id === ingredient._id)
+			count = 2
+
+		return count
+	}, [selectIngredients, bun])
 
 	return (
 		<li key={ingredient._id} className={clsx(s.item)} onClick={() => dispatch(openModal({item: ingredient}))} ref={ref}>
